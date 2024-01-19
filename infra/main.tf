9259b3b2c9401c7542b1a411c95322a939458823
mvgv <mvgv1989@gmail.com>
2024-01-19 01:02:35 -0300

    fix: deixando endpoint publico

--- infra/main.tf
+++ infra/main.tf
@@ -45,6 +45,7 @@ module "eks" {
  cluster_name  = "appfastfood-cluster-teste"
  cluster_version = "1.28"
  cluster_endpoint_private_access = false
+ cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
