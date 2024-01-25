679a275e833a86b391951e1fecbed544459ad98c
mvgv <mvgv1989@gmail.com>
2024-01-24 23:34:46 -0300

    fix: criando recursos de rede manualmente

--- infra/main.tf
+++ infra/main.tf
@@ -1,33 +1,96 @@
 provider "aws" {
-  region = "us-east-1"
+ region = "us-east-1"
+}
+
+module "vpc" {
+ source = "terraform-aws-modules/vpc/aws"
+ 
+
+ name = "my-vpc"
+ cidr = "10.0.0.0/16"
+
+ azs           = ["us-east-1a", "us-east-1b"]
+ private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+ public_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+ enable_nat_gateway = true
+}
+
+resource "aws_security_group" "ssh_cluster" {
+  name        = "ssh_cluster"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "ssh_cluster_in" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
+  security_group_id = aws_security_group.ssh_cluster.id
+}
+
+resource "aws_security_group_rule" "ssh_cluster_out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
+  security_group_id = aws_security_group.ssh_cluster.id
 }
 
 module "eks" {
-  source        = "terraform-aws-modules/eks/aws"
-  version       = "19.0.0"
-  cluster_name  = "appfastfood-cluster-teste"
-  cluster_version = "1.28"
-  cluster_endpoint_private_access = false
-  cluster_endpoint_public_access = true
-  create_kms_key = false
+ source = "terraform-aws-modules/eks/aws"
+ version = "19.0.0"
+ cluster_name  = "appfastfood-cluster-prod"
+ cluster_version = "1.28"
+ cluster_endpoint_private_access = false
+ cluster_endpoint_public_access = true
+ create_kms_key = false
 
-  vpc_id     = "vpc-05ba31270003da29b"
-  subnet_ids = [
-    "subnet-03f7aa4b4780b89ad",  # my-vpc-private-us-east-1a
-    "subnet-02c982c370c77fd11",  # my-vpc-public-us-east-1a
-    "subnet-0f938a5d312e5c832",  # my-vpc-public-us-east-1b
-    "subnet-0cec6e18625316d62",  # my-vpc-private-us-east-1b
-  ]
-
-  cluster_encryption_config = {}
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     alura = {
-      min_size                = 1
-      max_size                = 10
-      desired_size            = 3
-      vpc_security_group_ids = ["sg-0ffe261cc2058834c"]
-      instance_types          = ["t2.micro"]
+      min_size     = 1
+      max_size     = 10
+      desired_size = 3
+      vpc_security_group_ids = [aws_security_group.ssh_cluster.id]
+      instance_types = ["t2.micro"]
     }
   }
+
+  cluster_encryption_config = {}
+  
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+ role      = "deploy_lambda_dynamo"
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
+ role      = "deploy_lambda_dynamo"
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
+
+resource "aws_iam_role_policy" "eks_nodegroup_policy" {
+  name = "eks_nodegroup_policy"
+  role = "deploy_lambda_dynamo"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "eks:*",
+      "Resource": "arn:aws:eks:us-east-1:101478099523:cluster/my-eks-cluster"
+    }
+  ]
+}
+EOF
 }
